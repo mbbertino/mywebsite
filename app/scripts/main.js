bb2f1ffa6fbc67d9a4ae550e54f2611d0d5bae6d
Matt Bertino <mattbertino@gmail.com>
2014-01-21 17:33:40 -0500

    final JQ end of day

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('\'Allo \'Allo!');
+
+// this java script impacts 5 different elements with 2 different states
+$('.link').click(function(){
+	console.log('linkcliked!!!');
+
+	$(this).addClass('clicknav');
+});
+
+//this is prob better one a css transition
+//impacts 3 elemts Total up to: 8Jquery examples
+$('.highlight').hover(function(){
+	console.log('worked');
+
+	$(this).addClass('hovbanner');
+});
+
+// 
+$('.smallimg').click(function(){
+	
+	console.log('linkcliked!!!');
+
+	$('.smallimg').not(this).addClass('hidding');
+
+	$(this).addClass('clickimg');
+
+});
+
+
