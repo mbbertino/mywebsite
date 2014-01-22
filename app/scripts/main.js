1288400b2a543d029441df12f49beb5cb61abdc2
Matt Bertino <mattbertino@gmail.com>
2014-01-21 23:10:07 -0500

    Initial dist subtree commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,15 +23,33 @@ $('.imgbox').click(function(){
 
 });
 
-
-
-$( ".imgbox" ).hover(function() {
-  $( ".caption" ).animate({
+$('.imgbox').hover(function() {
+  $('.caption').animate({
     opacity: 0.25,
-    height: "toggle",
-    bottom: "-=50",
+    height: 'toggle',
+    bottom: '-=50',
   }, 1000, function() {
     // Animation complete.
   });
 });
 
+$('.clickhere').click(function() {
+
+	$('.addbox').first().before('Did I just cut in line???')
+
+})
+
+$('.clickhere2').click(function() {
+
+	$('.addbox2').add('.addbox2').css( 'color', 'white' );
+	$('.addbox2').add('.addbox2').css( 'text-align', 'center' );
+	$('.addbox2').add('.addbox2').css( 'font-size', '45px' );
+	$('.addbox2').add('.addbox2').css( 'background-color', 'black' );
+
+})
+
+$('.clickhere3').click(function() {
+
+	$('.addbox3').add('.addbox3').css( 'color', 'white' );
+	$('.addbox3').first().after(', WAIT NO ITS NOT ?!?!?!')
+})
